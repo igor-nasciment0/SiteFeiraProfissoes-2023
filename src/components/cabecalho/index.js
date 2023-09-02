9bdebd707ef6ab8efb22b3819c0cd90d8eb8697e
igao <nascimentoigor1311@gmail.com>
2023-09-02 12:32:51 -0300

    cabecalho pronto

--- src/components/cabecalho/index.js
+++ src/components/cabecalho/index.js
@@ -3,9 +3,13 @@ import './index.scss';
 export default function Cabecalho() {
     return(
         <header className="cabecalho-usuario">
-            <div className="logo">
-                <img src="" alt="" />
+            <div className="container-logo">
+                <img src="/assets/images/logo.png" alt="Logo do Instituto" />
             </div> 
+
+            <h1>Feira de Profissões 2023</h1>
+
+            <h2>Instituto Social Nossa Senhora de Fátima</h2>
         </header>
     )
 }
