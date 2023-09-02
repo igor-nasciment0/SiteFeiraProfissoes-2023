216dc831a404238fe60988a3485caf135e5e2a9c
igao <nascimentoigor1311@gmail.com>
2023-09-02 13:21:31 -0300

    seção inicial pronta

--- src/pages/landpage/index.js
+++ src/pages/landpage/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Cabecalho from '../../components/cabecalho';
 import './index.scss';
 
@@ -5,6 +6,22 @@ export default function Landpage() {
     return(
         <div className="pagina-landpage">
             <Cabecalho />
+
+            <section className="sec-inicio">
+                <div className="container-inicio">
+                    <div>
+                        <h1>Te esperamos na</h1>
+                        <h2>Feira de Profissões 2023</h2>
+                        <Link>Inscreva-se Agora</Link>
+                    </div>
+
+                    <div className="data">
+                        <h3>Dia 30</h3>
+                        <h4>Setembro</h4>
+                        <p>Das 09h às 15h</p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
