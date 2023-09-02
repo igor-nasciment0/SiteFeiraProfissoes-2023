84fcc73210fa6aae187cff275cdf57a3066fe7d5
igao <nascimentoigor1311@gmail.com>
2023-09-02 16:34:06 -0300

    seções de texto prontas

--- src/pages/landpage/index.js
+++ src/pages/landpage/index.js
@@ -35,7 +35,7 @@ export default function Landpage() {
 
             <section className="sec-tempo">
                 <div className="container-tela">
-                    <h1>Restam:</h1>
+                    <h1>Faltam:</h1>
 
                     <div className="contador">
                         <div>
@@ -55,6 +55,42 @@ export default function Landpage() {
                             <h4>Segundos</h4>
                         </div>
                     </div>
+                </div>
+            </section>
+
+            <section className="sec-sobre">
+                <div className="container-tela">
+                    <div>
+                        <h2>Na nossa feira de profissões,</h2>
+                        <h3>Mais de mil visitantes por ano</h3>
+
+                        <p>Há 3 anos realizamos em nosso instituto a Feira de Profissões, um evento para todas as famílias para que <span>conheçam um pouco do que temos feito por nossos alunos.</span></p>
+                        <p>Neste evento nossos alunos mostrarão seus projetos usando tudo o que aprenderam aqui, querendo mostrar o melhor que se há de oferecer no FREI. </p>
+                        <p>Além de poder participar e observar os projetos, pode ser uma oportunidade para conhecer melhor aquilo que gosta e deseja para seu futuro.</p>
+                    </div>
+
+                    <img src="/assets/images/imagem-feira.png" alt="Imagem da Feira de 2021" />
+                </div>
+            </section>
+
+            <div className="faixa-img">
+                <img src="/assets/images/FaixaRetangulo.png" alt="" />
+            </div>
+
+            <section className="sec-conheca">
+                <div className="container-tela">
+                    <div>
+                        <h2>Venha conhecer nossa instituição e os projetos desenvolvidos por nossos alunos.</h2>
+                        <p>Na Feira de Profissões, você terá oportunidade de conhecer os cursos oferecidos pelo Instituto por meio de projetos criativos feitos pelos próprios alunos para demonstrarem seu conhecimento.</p>
+                    </div>
+                </div>
+            </section>
+
+            <section className="faixa-inscreva">
+                <div>
+                    <h2>Está gostando?</h2>
+                    <h3>Não perca tempo.</h3>
+                    <Link>Inscreva-se Agora</Link>
                 </div>
             </section>
         </div>
