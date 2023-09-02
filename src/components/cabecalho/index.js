38591b1540c7cb6b4d17fc9900888deb0d19b0ef
igao <nascimentoigor1311@gmail.com>
2023-09-02 12:11:33 -0300

    pastas e esqueleto do projeto

---
+++ src/components/cabecalho/index.js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+export default function Cabecalho() {
+    return(
+        <header className="cabecalho-usuario">
+            <div className="logo">
+                <img src="" alt="" />
+            </div> 
+        </header>
+    )
+}
---
+++ src/pages/landpage/index.js
@@ -0,0 +1,10 @@
+import Cabecalho from '../../components/cabecalho';
+import './index.scss';
+
+export default function Landpage() {
+    return(
+        <div className="pagina-landpage">
+            <Cabecalho />
+        </div>
+    )
+}
