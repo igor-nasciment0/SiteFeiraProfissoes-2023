5eae568b9b42886ab28baa446cac15bfda5bb236
Macgithub13 <macgames444@gmail.com>
2023-09-06 00:24:10 -0300

    home-adm

--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ import Landpage from './pages/landpage';
 import Inscricao from './pages/inscricao';
 import Reservado from './pages/ingressoreservado';
 import LoginAdm from './pages/adm/login-adm';
+import HomeAdm from './pages/adm/home-adm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landpage/>}/>
-        <Route path='/inscricao' element={<Inscricao/>}/>
-        <Route path='/reservado' element={<Reservado/>}/>
-        <Route path='/login-adm' element={<LoginAdm/>}/>
+        <Route path='/' exact={true} element={<Landpage/>}/>
+        <Route path='/inscricao' exact={true} element={<Inscricao/>}/>
+        <Route path='/reservado' exact={true} element={<Reservado/>}/>
+        <Route path='/adm/login' exact={true} element={<LoginAdm/>}/>
+        <Route path='/adm' exact={true} element={<HomeAdm/>}/>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
