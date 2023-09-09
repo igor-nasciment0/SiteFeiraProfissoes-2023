9cf0b1dd1c77985fca393b3923851a07f4ef8bbc
Macys1704 <cherin1704@gmail.com>
2023-09-09 01:52:51 -0300

    terminando

--- src/pages/inscricao/index.js
+++ src/pages/inscricao/index.js
@@ -10,11 +10,9 @@ import Cabecalho from '../../components/cabecalho';
 export default function Inscricao() {
 
   const [nome, setNome] = useState('')
-  const [chamado, setChamado] = useState('')
   const [email, setEmail] = useState('')
   const [telefone, setTelefone] = useState('')
   const [bairro, setBairro] = useState('')
-  const [previsao, setPrevisao] = useState('')
   const [sabendo, setSabendo] = useState('')
   const [foialuno, setFoialuno] = useState('')
 
@@ -24,7 +22,7 @@ export default function Inscricao() {
 
      // Verifica se os campos obrigatórios estão preenchidos
     
-     if (nome && chamado && email && telefone && bairro && previsao && sabendo && foialuno) {
+     if (nome && email && telefone && bairro && sabendo && foialuno) {
 
       // Exibe um alerta de confirmação antes de salvar
       confirmAlert({
@@ -36,11 +34,9 @@ export default function Inscricao() {
             onClick: async () => {
               let inscricao = {
                 nm_nome: nome,
-                nm_ser_chamado: chamado,
                 ds_email: email,
                 nr_telefone: telefone,
                 nm_bairro: bairro,
-                ds_prev_chegada: previsao,
                 ds_sabendo: sabendo,
                 ds_foialuno: foialuno,
               };
@@ -94,12 +90,6 @@ export default function Inscricao() {
               <input value={nome} onChange={e => setNome(e.target.value)} placeholder="Ex: João Lucas da Silva"></input>
             </div>
 
-
-            <div>
-              <h1>Como gostaria de ser chamado?</h1>
-              <input value={chamado} onChange={e => setChamado(e.target.value)} placeholder="Ex: João"></input>
-            </div>
-
             <div>
               <h1>E-MAIL</h1>
               <InputMask type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Ex: nome123@gmail.com" maskPlaceholder={setEmail} required />
@@ -117,10 +107,6 @@ export default function Inscricao() {
               <input value={bairro} onChange={e => setBairro(e.target.value)} placeholder="Ex: Cocaia"></input>
             </div>
 
-            <div>
-              <h1>Previsão de chegada:</h1>
-              <input value={previsao} onChange={e => setPrevisao(e.target.value)} placeholder="Ex: 10:30"></input>
-            </div>
 
             <div>
               <h1>Como ficou sabendo da feira ?</h1>
