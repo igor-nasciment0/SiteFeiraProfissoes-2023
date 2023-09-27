437806213d296de405cf364debf98dbd22b464bd
igor-nasciment0 <141491741+igor-nasciment0@users.noreply.github.com>
2023-09-27 09:05:26 -0300

    último link tela inicial

--- src/pages/landpage/index.js
+++ src/pages/landpage/index.js
@@ -40,7 +40,7 @@ export default function Landpage() {
     [
         {pergunta: 'Sobre a alimentação', resposta:'No prédio do Instituto, você também encontrará a nossa lanchonete. Bom apetite!'},
         {pergunta: 'Empresas convidadas', resposta:'Estas são algumas das empresas convidadas para nosso evento:', lista: ['Casa da Mulher Paulistana (Doação de Cabelo)', 'Universidade Ítalo-Brasileiro', 'UNISA', 'STB Intercâmbio', 'Paraoficina', 'SupplyTech', 'Daniela Matos (Nutricionista)', 'São Paulo Open Centre', 'Seguradora Maphre']},
-        {pergunta: 'Como fazer a inscrição?', resposta:'Preencha o formulário ', link: {texto: 'neste link', url:'/inscricao'}},
+        {pergunta: 'Como fazer a inscrição?', resposta:'Preencha o formulário ', link: {texto: 'neste link.', url:'https://forms.gle/WR157key1EyZubcDA'}},
     ]
 
     return(
